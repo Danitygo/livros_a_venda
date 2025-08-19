4560892739a51f046a41637dbc421cb211762547
DanielNaty <naty@gmail.com>
2025-08-20 00:26:14 +0100

    Corrigindo import no cabecalho e page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./page.module.css";
-import VisaoGeral from "./componentes/visaoGeral/visao";
-import Secao from "./componentes/secao/secao";
-import Contato from "./componentes/contato/contato";
-import Cabecalho from "./componentes/cabeclho/cabecalho";
-import SobreNos from "./componentes/sobreNos/sobre";
+import Image from 'next/image'
+import { Link } from 'react-router-dom';
+import estilos from './page.module.css'
+import Cabecalho from './componentes/cabecalho/cabecalhos';
+import VisaoGeral from './componentes/visaoGeral/visao';
+import Secao from './componentes/secao/secao';
+import SobreNos from './componentes/sobreNos/sobre';
+import Contato from './componentes/contato/contato';
+
 
 export default function Home() {
   return (

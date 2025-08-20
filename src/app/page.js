3825ec881a67dec714f4af7be6b841330518f1b0
DanielNaty <naty@gmail.com>
2025-08-20 01:06:35 +0100

    renomiado a componente sobreNos

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import { Link } from 'react-router-dom';
+import Image from 'next/image';
+import Link from 'next/link';
 import estilos from './page.module.css'
 import Cabecalho from './componentes/cabecalho/cabecalhos';
 import VisaoGeral from './componentes/visaoGeral/visao';
 import Secao from './componentes/secao/secao';
-import SobreNos from './componentes/sobreNos/sobre';
 import Contato from './componentes/contato/contato';
 
 
